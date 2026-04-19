f793dd383ca6f31d7879ca93565100f33b986d64
KALPANA1005 <kalpana.m87787807@gmail.com>
2026-04-19 15:10:41 +0530

    update api url for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://onlineserviceeffectivenessdashboard-production.up.railway.app/api' });
 
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user') || '{}');
